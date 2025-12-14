8be16d427c0d33ee96c2af64ab8535ab055d64ee
diaachenko <dacenkonazar3@gmail.com>
2025-12-14 17:42:58 +0200

    Create CardboardBox.java

---
+++ pp-lab-4-8/src/Model/Cardboard/CardboardBox.java
@@ -0,0 +1,6 @@
+package Model.Cardboard;
+import Model.Package.GiftContainer;
+
+public class CardboardBox extends GiftContainer {
+    public CardboardBox(String color, int weight, int volume, int load) { super(color, weight, volume, load); }
+}
