ac1b4233efe0a24e08e23f7301f8eb3874dac9ca
diaachenko <dacenkonazar3@gmail.com>
2025-12-14 17:44:05 +0200

    Create Jar.java

---
+++ pp-lab-4-8/src/Model/Glass/Jar.java
@@ -0,0 +1,7 @@
+package Model.Glass;
+
+import Model.Package.GiftContainer;
+
+public class Jar extends GiftContainer {
+    public Jar(String color, int weight, int volume, int load) { super(color, weight, volume, load); }
+}
