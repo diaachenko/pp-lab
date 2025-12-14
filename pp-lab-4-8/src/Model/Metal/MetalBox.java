fe577289a882ce9fc3c432316e8178ceb6e4c790
diaachenko <dacenkonazar3@gmail.com>
2025-12-14 17:43:32 +0200

    Create MetalBox.java

---
+++ pp-lab-4-8/src/Model/Metal/MetalBox.java
@@ -0,0 +1,7 @@
+package Model.MetalBox;
+
+import Model.Package.GiftContainer;
+
+public class MetalBox extends GiftContainer {
+    public MetalBox(String color, int weight, int volume, int load) { super(color, weight, volume, load); }
+}
