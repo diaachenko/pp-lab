a3e43dd77330de5aa67e934fcb9173a37b30ee2c
diaachenko <dacenkonazar3@gmail.com>
2025-12-14 17:42:24 +0200

    Create PaperBag.java

---
+++ pp-lab-4-8/src/Model/Paper/PaperBag.java
@@ -0,0 +1,7 @@
+package Model.Paper;
+
+import Model.Package.GiftContainer;
+
+public class PaperBag extends GiftContainer {
+    public PaperBag(String name, int weight, int volume, int load) { super(name, weight, volume, load); }
+}
